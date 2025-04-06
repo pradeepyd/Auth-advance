b5d93f670027e002d65728a7c67f6d7797176373
Pradeep yadav <pradeep20020102@gmail.com>
2025-04-06 14:48:10 +0545

    server action for login

---
+++ actions/login.ts
@@ -0,0 +1,8 @@
+"use server";
+
+import * as z from "zod"
+import { LoginSchema } from "@/schemas";
+
+export const login = async(values:z.infer<typeof LoginSchema>) => {
+    console.log(values)
+}
