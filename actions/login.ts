ee097e1a8536e4ad9e45405733cc27a2ccb2c83e
Pradeep yadav <pradeep20020102@gmail.com>
2025-04-07 23:52:10 +0545

    server action for login done

--- actions/login.ts
+++ actions/login.ts
@@ -4,5 +4,11 @@ import * as z from "zod"
 import { LoginSchema } from "@/schemas";
 
 export const login = async(values:z.infer<typeof LoginSchema>) => {
-    console.log(values)
-}
+    const validatedFields = LoginSchema.safeParse(values);
+
+    if(!validatedFields.success){
+        return {error: "Invalid fields!"};
+    }
+
+    return { success : "Email sent!"};
+};
