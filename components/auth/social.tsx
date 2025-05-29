0fc29bcefb274fba12c1ef727ed5c9a919238181
Pradeep yadav <pradeep20020102@gmail.com>
2025-05-29 09:03:30 +0545

    using signin in client side

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -3,15 +3,23 @@
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa6";
 import { Button } from "@/components/ui/button";
+import { signIn } from "next-auth/react";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 export const Social = () => {
+  const onClick = (providers:"google" | "github") => {
+    signIn(providers,{
+      callbackUrl: DEFAULT_LOGIN_REDIRECT,
+    });
+  }
+
   return (
     <div className=" grid grid-cols-2 items-center w-full gap-x-3">
       <Button
         size={"lg"}
         className="w-full"
         variant={"outline"}
-        onClick={() => {}}
+        onClick={() => onClick("google")}
       >
         <FcGoogle className="h-6 w-6" />
       </Button>
@@ -19,7 +27,7 @@ export const Social = () => {
         size={"lg"}
         className="w-full"
         variant={"outline"}
-        onClick={() => {}}
+        onClick={() => onClick("github")}
       >
         <FaGithub className="h-6 w-6" />
       </Button>
