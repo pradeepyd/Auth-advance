bdb0c7071b2f5fd4912c889d5b200bf39914fdf9
Pradeep yadav <pradeep20020102@gmail.com>
2025-05-31 08:29:22 +0545

    for reseting password mail functionailty added

--- lib/mail.ts
+++ lib/mail.ts
@@ -12,3 +12,14 @@ export const sendVerificationEmail = async ( email: string, token:string ) => {
         html:`<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`
     })
 }
+
+export const sendPasswordResetEmail = async ( email: string, token:string ) => {
+    const ResetLink = `http://localhost:3000/auth/new-password?token=${token}`;
+
+    await resend.emails.send({
+        from: "Onboarding@resend.dev",
+        to: email,
+        subject: "Reset your password",
+        html:`<p>Click <a href="${ResetLink}">here</a> to reset your password.</p>`
+    })
+}
