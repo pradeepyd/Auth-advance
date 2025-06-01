a3ed038f79e50eb4b691c7edc0a4c5f93afd0f8e
Pradeep yadav <pradeep20020102@gmail.com>
2025-06-01 21:57:20 +0545

    two factor token functions

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -1,7 +1,33 @@
+import crypto from "crypto";
 import { v4 as uuidv4 } from "uuid";
 import { prisma } from "./db";
 import { getVerificationTokenByEmail } from "@/data/verification-token";
 import { getPasswordResetTokenByEmail } from "@/data/password-reset-token";
+import { getTwoFactorTokenByEmail } from "@/data/two-factor-token";
+
+export const generateTwoFactorToken = async(email :string) => {
+  const token = crypto.randomInt(100_000, 1_000_000).toString();
+  const expires = new Date(new Date().getTime() + 900 * 1000);
+
+  const existingToken = await getTwoFactorTokenByEmail(email);
+  if(existingToken){
+    await prisma.twoFactorToken.delete({
+      where:{
+        id:existingToken.id,
+      }
+    });
+  }
+
+  const twoFactorToken = await prisma.twoFactorToken.create({
+    data:{
+      email,
+      token,
+      expires,
+    }
+  })
+
+  return twoFactorToken;
+}
 
 export const generateVerificationToken = async (email: string) => {
   const token = uuidv4();
