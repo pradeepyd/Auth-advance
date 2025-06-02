4fee2def13718bb9574b389c414f7cee6ada5f91
Pradeep yadav <pradeep20020102@gmail.com>
2025-06-02 22:57:17 +0545

    role access an d 2fa added

---
+++ next-auth.d.ts
@@ -0,0 +1,13 @@
+import { UserRole } from "./lib/generated/prisma";
+import { DefaultSession } from "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      // customField : string,
+      id: string;
+      role: UserRole;
+      isTwoFactorEnabled:boolean;
+    } & DefaultSession["user"];
+  }
+}
