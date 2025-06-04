ce0d15d4eef480783ac077432171195fec852abe
Pradeep yadav <pradeep20020102@gmail.com>
2025-06-04 11:40:07 +0545

    callbackurl added for social

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -5,11 +5,14 @@ import { FaGithub } from "react-icons/fa6";
 import { Button } from "@/components/ui/button";
 import { signIn } from "next-auth/react";
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { useSearchParams } from "next/navigation";
 
 export const Social = () => {
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl");
   const onClick = (providers:"google" | "github") => {
     signIn(providers,{
-      callbackUrl: DEFAULT_LOGIN_REDIRECT,
+      callbackUrl: callbackUrl ||  DEFAULT_LOGIN_REDIRECT,
     });
   }
 
