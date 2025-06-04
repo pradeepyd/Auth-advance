67afcc9d5155edcabfcf9a78968e55a73ebbfc95
Pradeep yadav <pradeep20020102@gmail.com>
2025-06-04 11:41:01 +0545

    settingschema added along with refine

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,4 +1,32 @@
+import { UserRole } from "@/lib/generated/prisma"
 import * as z from "zod"
+
+export const SettingSchema = z.object({
+    name:z.optional(z.string()),
+    role:z.enum([UserRole.ADMIN ,UserRole.USER]),
+    IsTwoFactorEnabled:z.optional(z.boolean()),
+    email : z.optional(z.string().email()),
+    password:z.optional(z.string().min(6)),
+    newPassword:z.optional(z.string().min(6)),
+})
+  .refine((data) => {
+    if(data.password && !data.newPassword) {
+        return false;
+    }
+    return true;
+  },{
+    message:"New password is required!",
+    path:["newPassword"]
+  })
+  .refine((data) => {
+    if(data.newPassword && !data.password) {
+        return false;
+    }
+    return true;
+  },{
+    message:"password is required!",
+    path:["password"]
+  })
 
 export const LoginSchema = z.object({
     email : z.string().email({
